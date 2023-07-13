4f38e7dd512cf88218ba3db62d0a7cb2f0b1e76a
Douglas Gemir <carvalhodouglas95@gmail.com>
2023-07-13 00:10:41 -0300

    principais rotas

--- api/index.py
+++ api/index.py
@@ -9,3 +9,23 @@ def root():
 @app.get("/produtos")
 def get_produtos():
     return {"message": "Hello World"}
+
+@app.post("/cadastrarProduto")
+def post_produto():
+    return {"message": "produto cadastrado"} 
+
+@app.patch("/editarProduto/{produto_id}")
+def update_produto():
+    return {"message": "produto atualizado"} 
+
+@app.get("/lerPedidos")
+def read_pedido():
+    return {"message": "lista de pedidos"}
+
+@app.post("/cadastrarUsuario")
+def post_produto():
+    return {"message": "usuário cadastrado"} 
+
+@app.patch("/editarUsuario/{usuario_id}")
+def update_produto():
+    return {"message": "usuario atualizado"} 
