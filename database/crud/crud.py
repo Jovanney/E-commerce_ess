7e08d87fe5386e8b3e1e2de776d8e07ba21f78a6
Jovanney <100790418+Jovanney@users.noreply.github.com>
2023-08-11 17:13:09 -0300

    feat(crud) Adiciona funcoes para criar e buscar usuarios

---
+++ database/crud/crud.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database.get_db import SessionLocal, get_db
+from database.models.modelos import Usuario
+from database.shemas.schemas import UsuarioCreate
+
+def get_user_by_email(usuario_email: str, db: SessionLocal = Depends(get_db)):
+    return db.query(Usuario).filter(Usuario.email == usuario_email).first()
+
+def create_user(db: Session, user: UsuarioCreate):
+    db_user = Usuario(email=user.email, senha=user.senha)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
