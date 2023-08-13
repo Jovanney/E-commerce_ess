c7514d4d733f0481d6d8b57a00624aa77deb5f2f
Jovanney <100790418+Jovanney@users.noreply.github.com>
2023-08-13 08:53:10 -0300

    feat(crud) criando funções de pegar e criar loja

--- database/crud/crud.py
+++ database/crud/crud.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database.get_db import SessionLocal, get_db
-from database.models.modelos import Usuario
-from database.shemas.schemas import UsuarioCreate
+from database.models.modelos import Loja, Usuario
+from database.shemas.schemas import LojaCreate, UsuarioCreate
 
 def get_user_by_email(usuario_email: str, db: SessionLocal = Depends(get_db)):
     return db.query(Usuario).filter(Usuario.email == usuario_email).first()
+
+def get_loja_by_cnpj(cnpj_loja: str, db: SessionLocal = Depends(get_db)):
+    return db.query(Loja).filter(Loja.cnpj == cnpj_loja).first()
+
+def create_loja_c(db: Session, loja: LojaCreate):
+    db_loja = Loja(**loja.dict())
+    db.add(db_loja)
+    db.commit()
+    db.refresh(db_loja)
+    return db_loja
 
 def create_user(db: Session, user: UsuarioCreate):
     db_user = Usuario(**user.dict())
