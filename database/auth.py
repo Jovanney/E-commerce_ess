1ddaa7b986a853ba97a87048a6de8b6d7aea7b27
Jovanney <100790418+Jovanney@users.noreply.github.com>
2023-08-14 13:18:50 -0300

    feat(auth) criando função que gera token jwt

--- database/auth.py
+++ database/auth.py
@@ -1,8 +1,14 @@
+from datetime import timedelta
+import datetime
 from fastapi import Depends
 from passlib.context import CryptContext
+from jose import JWTError, jwt
 
 from database.crud.crud import get_user_by_email
 from database.get_db import SessionLocal, get_db
+
+SECRET_KEY = "2b9297ddf50a5336ba333962928ce57a1db91464c45c1831d26a4e4b23f5889d"
+ALGORITHM = "HS256"
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -19,3 +25,13 @@ def authenticate_user(username: str, password: str, db: SessionLocal = Depends(g
     if not verify_password(password, user.hashed_password):
         return False
     return user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
