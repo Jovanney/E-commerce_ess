94c67abd80d6a92d2015282a21c99f26151ab2ab
Jovanney <100790418+Jovanney@users.noreply.github.com>
2023-08-27 13:58:35 -0300

    refactor(crud): removendo imports inutilizados

--- database/crud/crud.py
+++ database/crud/crud.py
@@ -1,26 +1,13 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session, joinedload
 from database.get_db import SessionLocal, get_db
-from database.models.modelos import Item, Pedido, Produto, Usuario
-from database.shemas.schemas import  ItemBase, ProdutoBase, UsuarioCreate
-from sqlalchemy.inspection import inspect
-from typing import Annotated
-from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
-from sqlalchemy.orm import Session
+from database.models.modelos import Item, Pedido, Produto, Usuario, Loja
+from database.shemas.schemas import UsuarioCreate, ProdutoCreate, LojaCreate
 import database.auth as auth
-from database.get_db import SessionLocal, get_db
-from database.models.modelos import Status, Usuario, Produto, Item
-from database.shemas.schemas import UsuarioCreate, ProdutoCreate
-from database.models.modelos import Pedido
-from database.models.modelos import Produto
-from sqlalchemy.orm import joinedload
-from sqlalchemy.inspection import inspect
-
-from database.models.modelos import Loja, Usuario
-from database.shemas.schemas import LojaCreate, UsuarioCreate
+from typing import Annotated
 from jose import JWTError, jwt
-from database.shemas.schemas import Token
+
 
 SECRET_KEY = "2b9297ddf50a5336ba333962928ce57a1db91464c45c1831d26a4e4b23f5889d"
 ALGORITHM = "HS256"
@@ -66,8 +53,6 @@ def create_prod(db: Session, produto: ProdutoCreate):
     db.refresh(db_prod)
     return db_prod
 
-
-#Delete de colocar itens no menu principal da sua loja
 def delete_produto_by_id(db: Session, produto_id: int):
     db_produto = db.query(Produto).filter(Produto.id_produto == produto_id).first()
     if db_produto is None:
@@ -75,10 +60,8 @@ def delete_produto_by_id(db: Session, produto_id: int):
     db.delete(db_produto)
     db.commit()
 
-    
 
-# Função para atualizar um produto na entidade Produto
-def update_produto(db: Session, produto_id: int, new_ats: list): # Colocar o autenticação(so a loja do seu cnpj pode alterar seus produtos)
+def update_produto(db: Session, produto_id: int, new_ats: list): 
     db_produto = db.query(Produto).filter(Produto.id_produto == produto_id).first()
     if db_produto:
         atributos = ['categoria_prod', 'nome_produto', 'marca_produto', 'preco', 'especificacoes']
@@ -90,8 +73,6 @@ def update_produto(db: Session, produto_id: int, new_ats: list): # Colocar o aut
         db.refresh(db_produto)
 
     return db_produto
-
-
 
 def to_dict(obj):
     if isinstance(obj, Pedido):
@@ -124,7 +105,7 @@ def get_pedidos_by_usuario(cpf_usuario: str, db: Session):
 
 
 
-# Cancelamento de Pedidos
+
 def cancelar_pedido(pedido_id: int, cpf_usuario: str, db: Session):
     # Primeiro, verificamos se o pedido existe e pertence ao CPF fornecido
     pedido = db.query(Pedido).filter(Pedido.id_pedido == pedido_id, Pedido.cpf_usuario == cpf_usuario).first()
@@ -146,7 +127,7 @@ def cancelar_pedido(pedido_id: int, cpf_usuario: str, db: Session):
         raise HTTPException(status_code=403, detail="Pedido não pode mais ser cancelado")
     
 
-#ITI
+
 def create_pedido_not_confirmed(db: Session, cpf_user: str):
     db_pedido = Pedido()
     db_pedido.cpf_usuario = cpf_user
@@ -157,7 +138,7 @@ def create_pedido_not_confirmed(db: Session, cpf_user: str):
     db.refresh(db_pedido)
     return db_pedido
 
-#ITI
+
 def create_item(db: Session, produto: Produto, pedido: Pedido, quantidade: int):
     db_item = Item()
     db_item.quantidade = quantidade
@@ -168,34 +149,34 @@ def create_item(db: Session, produto: Produto, pedido: Pedido, quantidade: int):
     db.refresh(db_item)
     return db_item
 
-#DODO
+
 def update_total_price(db: Session, produto: Produto, pedido: Pedido, quantidade: int):
     pedido.preco_total = float(pedido.preco_total) + (float(produto.preco)*quantidade)
     db.commit()
 
-#ITI
+
 def get_produto(id_produto: int, db: SessionLocal = Depends(get_db)):
     return db.query(Produto).filter(Produto.id_produto == id_produto).first()
 
-#ITI 
+
 def get_item(id_produto: int, id_pedido: int, db: SessionLocal = Depends(get_db)):
     return db.query(Item).filter((Item.id_pedido == id_pedido) & (Item.id_produto == id_produto)).first()
 
-#Dodo
+
 def get_pedidos_by_status(status: int,cpf_user:str ,db: SessionLocal = Depends(get_db)):
     return  db.query(Pedido).filter((Pedido.pedido_status == status) & (Pedido.cpf_usuario == cpf_user)).first()
-#Dodo
+
 def get_itens_by_pedidos(pedido: Pedido, db: SessionLocal = Depends(get_db)):
     return db.query(Item).filter(Item.id_pedido == pedido.id_pedido).all()
 
-#ITI
+
 def update_quantidade_item(item: Item, db: SessionLocal = Depends(get_db)):
     db.merge(item)
     db.commit()
     db.refresh(item)
     return item
 
-def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: SessionLocal = Depends(get_db)):
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: SessionLocal = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -209,7 +190,7 @@ def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: SessionL
     except JWTError:
         raise credentials_exception
 
-    user = get_user_by_email(db = db, usuario_email = email)
+    user = get_user_by_email(db = db, email_user = email)
     store = get_loja_by_email(db = db, email_loja= email)
     entity = user if user else store
     if entity is None:
@@ -225,6 +206,7 @@ def update_user_password(db, user, new_password):
 def delete_user(db, user):
     db.delete(user)
     db.commit()
+    
 def get_pedidos_by_status(status: int,cpf_user:str ,db: SessionLocal = Depends(get_db)):
     return  db.query(Pedido).filter((Pedido.pedido_status == status) & (Pedido.cpf_usuario == cpf_user)).first()
 
