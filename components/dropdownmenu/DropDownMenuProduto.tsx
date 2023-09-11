90215809ee66f148dc781d79bbb1e36478ff32b8
Jovanney <100790418+Jovanney@users.noreply.github.com>
2023-09-11 15:53:27 -0300

    feat: adicionando componente dropdown que mostra as opções de produto

---
+++ components/dropdownmenu/DropDownMenuProduto.tsx
@@ -0,0 +1,26 @@
+import {
+    GanttChartSquare,
+    UserPlus
+  } from "lucide-react"
+  
+  import { Button, buttonVariants } from "@/components/ui/button"
+  import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+import Link from "next/link"
+  
+  export function DropdownMenuProduto() {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger className={buttonVariants()}>Produtos&ensp;<GanttChartSquare size={30}/></DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem><Link href="/">Adicionar Produto&ensp;</Link></DropdownMenuItem>
+                <DropdownMenuItem><Link href="/">Gerenciar Produtos&ensp;</Link></DropdownMenuItem>
+                </DropdownMenuContent>
+        </DropdownMenu>
+    )
+  }
+  
