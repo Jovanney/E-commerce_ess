e385dda6b9534fdbadd167f8e02d7e2d8101b020
Vitor Leitao <vml2@cin.ufpe.br>
2023-09-12 23:23:26 -0300

    feat: Adicionando o link para as paginas de gerenciar produto e adicionar produto ao menu

--- components/dropdownmenu/DropDownMenuProduto.tsx
+++ components/dropdownmenu/DropDownMenuProduto.tsx
@@ -1,26 +1,25 @@
 import {
-    GanttChartSquare,
-    UserPlus
-  } from "lucide-react"
-  
-  import { Button, buttonVariants } from "@/components/ui/button"
-  import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu"
+  GanttChartSquare,
+  UserPlus
+} from "lucide-react"
+
+import { Button, buttonVariants } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import Link from "next/link"
-  
-  export function DropdownMenuProduto() {
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger className={buttonVariants()}>Produtos&ensp;<GanttChartSquare size={30}/></DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuItem><Link href="/">Adicionar Produto&ensp;</Link></DropdownMenuItem>
-                <DropdownMenuItem><Link href="/">Gerenciar Produtos&ensp;</Link></DropdownMenuItem>
-                </DropdownMenuContent>
-        </DropdownMenu>
-    )
-  }
-  
+
+export function DropdownMenuProduto() {
+  return (
+      <DropdownMenu>
+          <DropdownMenuTrigger className={buttonVariants()}>Produtos&ensp;<GanttChartSquare size={30}/></DropdownMenuTrigger>
+          <DropdownMenuContent>
+              <DropdownMenuItem><Link href="/create">Adicionar Produto&ensp;</Link></DropdownMenuItem>
+              <DropdownMenuItem><Link href="/update">Gerenciar Produtos&ensp;</Link></DropdownMenuItem>
+              </DropdownMenuContent>
+      </DropdownMenu>
+  )
+}
